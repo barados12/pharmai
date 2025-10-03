b1088a0302a33392a42713547132a1384cd00fc2
barados12 <elgarnaouijamal143@gmail.com>
2025-10-03 21:18:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -408,4 +408,5 @@ def health():
     return jsonify({"status":"ok","ts": datetime.utcnow().isoformat()})
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=PORT)
+#    app.run(host="0.0.0.0", port=PORT)
+     app.run()
